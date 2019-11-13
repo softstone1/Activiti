e1cd2195fecf168c150dc496487b21a9aa2a4473
Miguel Ruiz <miguel.ruiz.developer@gmail.com>
2019-11-13 18:44:38 +0000

    AAE-785 Fix h2 db create script

--- activiti-engine/src/main/resources/org/activiti/db/create/activiti.h2.create.engine.sql
+++ activiti-engine/src/main/resources/org/activiti/db/create/activiti.h2.create.engine.sql
@@ -208,7 +208,7 @@ create table ACT_RU_TASK (
     TENANT_ID_ varchar(255) default '',
     FORM_KEY_ varchar(255),
     CLAIM_TIME_ timestamp,
-    APP_VERSION integer,
+    APP_VERSION_ integer,
     primary key (ID_)
 );
 
