609a8cda9608c1633b031c18af3e6a105ca5fdba
Zoltan Palfi <zoltan.palfi@alfresco.com>
2019-11-14 13:44:53 +0000

    AAE-785: Fixed tests, minor refactor

--- activiti-engine/src/main/java/org/activiti/engine/impl/cmd/NewTaskCmd.java
+++ activiti-engine/src/main/java/org/activiti/engine/impl/cmd/NewTaskCmd.java
@@ -13,38 +13,37 @@
 package org.activiti.engine.impl.cmd;
 
 import java.io.Serializable;
+import java.util.Optional;
 
 import org.activiti.engine.impl.interceptor.Command;
 import org.activiti.engine.impl.interceptor.CommandContext;
 import org.activiti.engine.impl.persistence.entity.DeploymentEntity;
+import org.activiti.engine.impl.persistence.entity.DeploymentEntityManager;
 import org.activiti.engine.impl.persistence.entity.TaskEntity;
 import org.activiti.engine.task.Task;
 
-/**
-
- */
 public class NewTaskCmd implements Command<Task>, Serializable {
 
-  private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-  protected String taskId;
+    private final String taskId;
 
-  public NewTaskCmd(String taskId) {
-    this.taskId = taskId;
-  }
-
-  public Task execute(CommandContext commandContext) {
-    TaskEntity task = commandContext.getTaskEntityManager().create();
-    task.setId(taskId);
-    task.setRevision(0);
-
-    DeploymentEntity deploymentEntity = commandContext.getDeploymentEntityManager().findLatestDeploymentByName("SpringAutoDeployment");
-
-    if(deploymentEntity.getVersion() != null) {
-        task.setAppVersion(deploymentEntity.getVersion());
+    public NewTaskCmd(String taskId) {
+        this.taskId = taskId;
     }
 
-    return task;
-  }
+    public Task execute(CommandContext commandContext) {
+        TaskEntity task = commandContext.getTaskEntityManager().create();
+        task.setId(taskId);
+        task.setRevision(0);
+        findAppVersionFromDeployment(commandContext.getDeploymentEntityManager())
+                .ifPresent(task::setAppVersion);
+        return task;
+    }
 
+    private Optional<Integer> findAppVersionFromDeployment(DeploymentEntityManager deploymentEntityManager) {
+        DeploymentEntity deployment = deploymentEntityManager.findLatestDeploymentByName("SpringAutoDeployment");
+        return Optional.ofNullable(deployment)
+                .map(DeploymentEntity::getVersion);
+    }
 }
--- activiti-spring-boot-starter/src/test/java/org/activiti/spring/boot/tasks/TaskRuntimeAppVersionTest.java
+++ activiti-spring-boot-starter/src/test/java/org/activiti/spring/boot/tasks/TaskRuntimeAppVersionTest.java
@@ -4,6 +4,7 @@ import org.activiti.api.runtime.shared.query.Page;
 import org.activiti.api.runtime.shared.query.Pageable;
 import org.activiti.api.task.model.Task;
 import org.activiti.api.task.model.builders.TaskPayloadBuilder;
+import org.activiti.api.task.model.payloads.CreateTaskPayload;
 import org.activiti.api.task.runtime.TaskRuntime;
 import org.activiti.spring.boot.security.util.SecurityUtil;
 import org.activiti.spring.boot.test.util.TaskCleanUpUtil;
@@ -30,30 +31,26 @@ public class TaskRuntimeAppVersionTest {
     private TaskCleanUpUtil taskCleanUpUtil;
 
     @After
-    public void taskCleanUp(){
+    public void taskCleanUp() {
         taskCleanUpUtil.cleanUpWithAdmin();
     }
 
     @Test
-    public void appVersionShouldBePopulatedWhenTaskIsCreated(){
-
+    public void should_appVersionNotNull_when_taskIsCreated() {
         securityUtil.logInAs("garth");
 
-        Task standAloneTask = taskRuntime.create(TaskPayloadBuilder.create()
-                                                         .withName("task with appVersion- wololo")
-                                                         .build());
+        CreateTaskPayload newTask = TaskPayloadBuilder.create()
+                .withName("new task")
+                .build();
+        taskRuntime.create(newTask);
 
-        Page<Task> tasks = taskRuntime.tasks(Pageable.of(0,
-                                                         50));
-
+        Page<Task> tasks = taskRuntime.tasks(Pageable.of(0, 50));
         assertThat(tasks.getContent()).hasSize(1);
-        Task task = tasks.getContent().get(0);
 
-        assertThat(task.getStatus()).isEqualTo(Task.TaskStatus.CREATED);
-        assertThat(task.getName()).isEqualTo(standAloneTask.getName());
-        assertThat(task.getAppVersion()).isNotNull();
-        assertThat(task.getAppVersion()).isEqualTo("1");
+        Task actual = tasks.getContent().get(0);
 
+        assertThat(actual.getStatus()).isEqualTo(Task.TaskStatus.CREATED);
+        assertThat(actual.getName()).isEqualTo("new task");
+        assertThat(actual.getAppVersion()).isEqualTo("1");
     }
-
 }
